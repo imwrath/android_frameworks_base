29ff4c31c00916f72a18326b4dfac8b641bc6b2a
darkbeastx <vsushant8421@gmail.com>
2018-12-09 13:27:41 +0530

    Add Meteor Category

--- packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
@@ -51,6 +51,7 @@ public final class CategoryKey {
     public static final String CATEGORY_GESTURES = "com.android.settings.category.ia.gestures";
     public static final String CATEGORY_NIGHT_DISPLAY =
             "com.android.settings.category.ia.night_display";
+    public static final String CATEGORY_METEOR = "com.android.settings.category.ia.Meteor";
 
     public static final Map<String, String> KEY_COMPAT_MAP;
 
